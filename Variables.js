3df5cc9c5941674e3c9238f02341d29d661534bd
Basitweb <basitrajput1011@gmail.com>
2025-07-12 12:55:49 +0500

    Variables

---
+++ Variables.js
@@ -0,0 +1,22 @@
+const accountId = 123
+let accountEmail = "basitrajput1011@gmail.com"
+var accountPassword = "uibsdafi"
+city = "Hyderabad"
+
+
+
+// accountId = 23
+// We can not change the value of const
+
+accountEmail = "bbjwfkl"
+
+// we can change the value of let and var 
+// preffer to use let because in var there is no scope 
+
+console.log("ID =" ,  accountId)
+console.log("Email =" ,  accountEmail)
+console.log("Password =" , accountPassword )
+console.log("City =" , city )
+
+
+console.table([accountId ,accountEmail ,accountPassword, city])
