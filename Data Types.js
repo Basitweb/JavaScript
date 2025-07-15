e9705e1bba605d65214e9022f74a5429e6be3a49
Basitweb <basitrajput1011@gmail.com>
2025-07-15 10:45:11 +0500

    Data Types

--- Data Types.js
+++ Data Types.js
@@ -1,1 +1,48 @@
-Dattagtalksjdbksdsfksfklsj;
+// "use strict "
+// alert(3+3);
+
+// we use node not web browser
+
+// we avoid ; in javascript 
+// console.log("Basit") console.log(6+6)
+// this is not a good practice
+
+// Documentation From Mdn Mozilla and acma sript
+
+//  Data Types in Java SScript
+
+let name = "Basit"
+// String
+let age = 18
+// number
+// BigInt
+let is_Loggedin = true
+// Boolean
+
+let money = null ;
+// stand alone value it mean did not have value 
+let mind  = undefined 
+// undefined 
+// abhi value assign nhi hoi 
+
+// symbol>>>
+// for unique 
+
+
+
+
+// non premative data type 
+
+// Object
+
+
+console.log(typeof(name))
+// string >> string
+console.log(typeof(age ))
+// number >> number 
+console.log(typeof(is_Loggedin))
+// boolean >> boolean
+console.log(typeof(money))
+// null >> Object 
+console.log(typeof(mind))
+// undefined >> undefined ;
