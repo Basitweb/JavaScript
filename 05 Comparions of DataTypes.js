2f13af7331d0142c3885c8c9f37a50a816858ebe
Basitweb <basitrajput1011@gmail.com>
2025-07-15 11:24:14 +0500

    05 Comparision of DAta Types

---
+++ 05 Comparions of DataTypes.js
@@ -0,0 +1,31 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+// true
+// true
+// false
+// false
+// false
+// true
+
+
+console.log("2" > 1 );
+// true 
+console.log("02" > 1 );
+// true 
+// \
+console.log(null > 0 );
+console.log(null == 0 );
+console.log(null >= 0 );
+// false
+// false
+// true
+
+console.log(undefined == 0);
+
+
+// === strict check 
+// check data type also 
