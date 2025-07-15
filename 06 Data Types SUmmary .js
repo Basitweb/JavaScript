93418c1a742f8b8ab8e211cc5fa34c0cde8d4a89
Basitweb <basitrajput1011@gmail.com>
2025-07-15 11:36:44 +0500

    Data Types Summary

---
+++ 06 Data Types SUmmary .js
@@ -0,0 +1,33 @@
+// 1 Premative 
+// 2 Non Premative 
+
+// Differnce  on the basis of
+// call by value or call by refrence 
+// how data is is stored in a memory
+
+// ********// Premative 
+// 7 catogries 
+// 1 String 
+// 2 Number 
+// 3 Bollean 
+// 4 Null 
+// 5 undefiened 
+// 6 BigInt
+// 7 Symbol
+ 
+// ********* Noon Premative / refrence 
+
+// 1 Arrays 
+// 2 Objects 
+// 3 Function
+
+// Q# Is Java Script is Dynamically Key typeof
+// or Statically Type 
+
+// Java Script is Dynamically 
+
+const heros = [ "batman" , "Superman"] 
+const myobj = {
+    name : "basit", 
+    age : 18, 
+}
