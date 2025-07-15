71285f4eb5dccfcefea440081aa656628c767dc7
Basitweb <basitrajput1011@gmail.com>
2025-07-15 11:17:12 +0500

    04 why String Conversion is Confusing

---
+++ 04 Why String to Number confusion .js
@@ -0,0 +1,56 @@
+let value = 3 
+
+let negvalue =-value
+
+console.log(value);
+// 3 
+console.log(negvalue);
+// -3
+
+console.log(2+2);
+// 4
+console.log(2-2);
+// 0
+console.log(2*2);
+// 4
+console.log(2/2);
+// 1
+console.log(2%2);
+// 0
+console.log(2**2);
+// 4
+
+
+let str1 = "Basit" ;
+let str2 = " rajput"
+
+let str3 = str1 + str2;
+
+console.log(str3);
+
+
+console.log(1 + "2");
+// 12                  
+console.log("1" + 2);
+// 12
+console.log("1" + "2");
+// 12
+console.log("1" + 2 + 2);
+// 122
+console.log(2+2+"2");
+// 42
+
+
+console.log(true);
+// true 
+console.log(+true);
+// 1
+console.log(+"");
+// 0
+
+let gameCounter = 100 
+console.log(gameCounter);
+// 100
+console.log(++gameCounter);
+// 101
+console.log(gameCounter++);
