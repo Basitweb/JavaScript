6c71888359f368a8ff16617a15ed43011fb60981
Basitweb <basitrajput1011@gmail.com>
2025-11-21 00:08:56 +0500

    Variables.js

---
+++ Variables.js
@@ -0,0 +1,12 @@
+// we can make variables in javascript by using different mehthood
+const accountId = 1;
+let userName = "Basit";
+var userAge = 9 ;
+
+
+// accountId = 88
+// we can not change const value 
+
+// we can change the value of let and var but we highly prefferd to use let
+
+console.table([accountId , userName , userAge]);
