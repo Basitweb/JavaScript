1d072eb6887ec01810e8ceb3c2912e7679ae6297
Basitweb <basitrajput1011@gmail.com>
2025-11-21 00:28:10 +0500

    02 Data types

---
+++ 02_Datatypes.js
@@ -0,0 +1,31 @@
+// There are 2 types of DataTypes 
+
+// 1.premative
+
+// number save numerical value
+// BigInt save lareger numerial value
+// Symbol to identify unique
+// String multiple characters
+// Boolean true / false
+// null standAlone value
+// undefined undefined
+
+
+// 2.Non - Premative
+
+// Object
+
+let userName = "Basit Rajput"
+let age = 20;
+let isActive = true;
+
+
+console.log(typeof 20);
+console.log(typeof "Basit Rajput");
+console.log(typeof Symbol);
+console.log(typeof BigInt(200202020020202002020202));
+console.log(typeof false);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
